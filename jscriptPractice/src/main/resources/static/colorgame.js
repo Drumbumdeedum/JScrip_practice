663c62a2afad4f214f0317fe965e60f679429306
Daniel Szalay <sza.dan.uj@gmail.com>
2017-08-04 13:27:22 +0200

    add new colors functionality

--- jscriptPractice/src/main/resources/static/colorgame.js
+++ jscriptPractice/src/main/resources/static/colorgame.js
@@ -3,6 +3,8 @@ var squares = document.querySelectorAll(".square");
 var pickedColor = pickRandomColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
 
 colorDisplay.textContent = pickedColor;
 
@@ -11,8 +13,10 @@ for (var i = 0; i < squares.length; i++) {
   squares[i].addEventListener("click", function(){
     var clickedColor = this.style.backgroundColor;
     if (clickedColor === pickedColor) {
-      messageDisplay.textContent = "Correct!"
+      resetButton.textContent = "Play again?";
+      messageDisplay.textContent = "Correct!";
       changeColors(clickedColor);
+      h1.style.backgroundColor = clickedColor;
     } else {
       this.style.backgroundColor = "#232323";
       messageDisplay.textContent = "Try Again!"
@@ -45,3 +49,14 @@ function randomColor() {
   var green = Math.floor(Math.random()* 256);
   return "rgb(" + red + ", " + green + ", " + blue + ")";
 }
+
+resetButton.addEventListener("click", function(){
+  colors = generateRandomColors(6);
+  pickedColor = pickRandomColor();
+  colorDisplay.textContent = pickedColor;
+  for (var i = 0; i < squares.length; i++){
+    squares[i].style.backgroundColor = colors[i];
+  }
+  h1.style.backgroundColor = "#232323";
+  resetButton.textContent = "New Colors";
+});
