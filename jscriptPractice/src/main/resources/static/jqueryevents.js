f60ae97a443b0afb3f179fca461e94dcca59fe70
Daniel Szalay <sza.dan.uj@gmail.com>
2017-08-08 21:11:25 +0200

    add keypress examples

--- jscriptPractice/src/main/resources/static/jqueryevents.js
+++ jscriptPractice/src/main/resources/static/jqueryevents.js
@@ -1,7 +1,13 @@
 $("button").click(function(){
-  alert("YOU CLICKED IT!!!"s);
+  alert("YOU CLICKED IT!!!");
 });
 
 $("button").click(function(){
   $(this).css("background", "pink");
+});
+
+$("input").keypress(function(event){
+  if(event.which === 13){
+    alert("YOU HIT ENTER!");
+  }
 });
