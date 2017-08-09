1815b415bedfeee70e9a1589e091b3ed8e0fb18a
Daniel Szalay <sza.dan.uj@gmail.com>
2017-08-09 20:52:30 +0200

    add fade in function

--- jscriptPractice/src/main/resources/static/jqueryeffects.js
+++ jscriptPractice/src/main/resources/static/jqueryeffects.js
@@ -1,3 +1,11 @@
-$('button').on("click", function(){
-  $('div').fadeOut(1000);
+$('.fadeOut').on("click", function(){
+  $('div').fadeOut(1000, function(){
+    console.log("Faded out!");
+  });
+});
+
+$('.fadeIn').on("click", function(){
+  $('div').fadeIn(1000, function(){
+    console.log("Faded out!");
+  });
 });
