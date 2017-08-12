c012ccb6157d0e70634a474fc3ec5e91a36d6e25
Daniel Szalay <sza.dan.uj@gmail.com>
2017-08-12 10:29:25 +0200

    update

--- jscriptPractice/src/main/resources/static/todoList.js
+++ jscriptPractice/src/main/resources/static/todoList.js
@@ -12,6 +12,6 @@ $("ul").on("click", "span", function(event){
 $("input[type='text']").keypress(function(event){
   if(event.which === 13){
     var text = $(this).val();
-    $("ul").append("<li><span>X</span> " + text + "</li>");
+    $("ul").append("<li><span><i class=\"fa fa-trash\"></i></span> " + text + "</li>");
   }
 });
